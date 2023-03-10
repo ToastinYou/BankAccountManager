f6abe66363dd78d8903675d1bd0307297bc6375a
Zach <zacharyhartt19@gmail.com>
2023-03-10 11:24:32 -0500

    implement basic command structure

--- Start.cs
+++ Start.cs
@@ -9,6 +9,28 @@
         private static void Main()
         {
             Console.WriteLine("Welcome to Logistics Banking: Mobile Banking Reimagined");
+            RequestCmd();
+        }
+
+        private static void RequestCmd()
+        {
+            Console.WriteLine("What would you like to do today?");
+            string cmd = Console.ReadLine();
+
+            switch (cmd)
+            {
+                case "help":
+                    Console.WriteLine("Requesting help..");
+                    break;
+                case "close":
+                    Console.WriteLine("Goodbye.");
+                    return;
+                default:
+                    Console.WriteLine("Invalid command, try again..");
+                    break;
+            }
+
+            RequestCmd();
         }
     }
 }
